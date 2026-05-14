290bc09a995804f6e279afe91b1699148604ce23
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-14 17:50:25 +0700

    membuat void peek

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -45,3 +45,21 @@ public:
         cout << "Popped value: " << top->data << endl;
         delete temp;
     }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
