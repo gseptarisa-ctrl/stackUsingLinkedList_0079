ba0806b6f3227f315cfc913f14023c2c92da0040
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-14 17:54:46 +0700

    membuat pilihan 2 else

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -100,4 +100,8 @@ int main()
             {
                 stack.pop();
             }
-          
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
